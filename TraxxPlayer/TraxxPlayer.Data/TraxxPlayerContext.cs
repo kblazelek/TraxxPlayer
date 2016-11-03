aaec10a9563915c00501f00413d2b6cdc4569d95
kblazelek <kblazelek@git.com>
2016-11-03 18:27:09 +0100

    Added TraxxPlayer.Data project

---
+++ TraxxPlayer/TraxxPlayer.Data/TraxxPlayerContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraxxPlayer.Data.Models;
+
+namespace TraxxPlayer.Data
+{
+    public class TraxxPlayerContext : DbContext
+    {
+        public DbSet<SoundCloudUser> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=TraxxPlayer.db");
+        }
+    }
+}
