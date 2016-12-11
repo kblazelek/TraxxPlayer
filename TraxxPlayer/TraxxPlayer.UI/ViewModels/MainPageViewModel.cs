85901cf2bd8bf88ec0fbc04fcff5cd37093904b3
kblazelek <kblazelek@git.com>
2016-12-11 21:11:49 +0100

    Added mostly played tracks to MainPage

--- TraxxPlayer/TraxxPlayer.UI/ViewModels/MainPageViewModel.cs
+++ TraxxPlayer/TraxxPlayer.UI/ViewModels/MainPageViewModel.cs
@@ -1,59 +1,48 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using Template10.Services.NavigationService;
+using TraxxPlayer.Common.Helpers;
+using TraxxPlayer.Common.Models;
+using TraxxPlayer.Services;
 using Windows.UI.Xaml.Navigation;
 
 namespace TraxxPlayer.UI.ViewModels
 {
     public class MainPageViewModel : CommonViewModel
     {
+        public ObservableCollection<SoundCloudTrack> RecentlyPopularTracks { get; set; } = new ObservableCollection<SoundCloudTrack>();
         public MainPageViewModel()
         {
-            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
-            {
-                Value = "Designtime value";
-            }
+            //var recentlyPopularTracks = TrackHistoryService.GetTracksHistory(App.User.id, 500).GroupBy(th => th.TrackID).Select(g => g.Count()).OrderByDescending();
+
         }
 
-        string _Value = "Gas";
-        public string Value { get { return _Value; } set { Set(ref _Value, value); } }
 
         public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> suspensionState)
         {
-            if (suspensionState.Any())
+            var recentlyPopularTracks =
+            (from th in TrackHistoryService.GetTracksHistory(App.User.id, 500)
+             group th by th.TrackID into g
+             select new { TrackID = g.Key, TrackCount = g.Count() }).Take(5);
+            foreach (var track in recentlyPopularTracks)
             {
-                Value = suspensionState[nameof(Value)]?.ToString();
+                var t = await SoundCloudHelper.GetSoundCloudTrack(track.TrackID);
+                RecentlyPopularTracks.Add(t);
             }
             await Task.CompletedTask;
         }
 
         public override async Task OnNavigatedFromAsync(IDictionary<string, object> suspensionState, bool suspending)
         {
-            if (suspending)
-            {
-                suspensionState[nameof(Value)] = Value;
-            }
             await Task.CompletedTask;
         }
 
         public override async Task OnNavigatingFromAsync(NavigatingEventArgs args)
         {
-            args.Cancel = false;
             await Task.CompletedTask;
         }
-
-        public void GotoDetailsPage() =>
-            NavigationService.Navigate(typeof(Views.DetailPage), Value);
-
-        public void GotoSettings() =>
-            NavigationService.Navigate(typeof(Views.SettingsPage), 0);
-
-        public void GotoPrivacy() =>
-            NavigationService.Navigate(typeof(Views.SettingsPage), 1);
-
-        public void GotoAbout() =>
-            NavigationService.Navigate(typeof(Views.SettingsPage), 2);
 
     }
 }
